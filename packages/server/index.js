1f2f26e9041e925cbc6871ae8c68e246356f899a
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-12-15 18:28:35 -0500

    12.socketio.sessions

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,47 +1,31 @@
 const express = require("express");
-const redisClient = require("./redis");
+const {
+  sessionMiddleware,
+  wrap,
+  corsConfig,
+} = require("./controllers/serverController");
 const { Server } = require("socket.io");
 const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
 const authRouter = require("./routers/authRouter");
-const session = require("express-session");
 const server = require("http").createServer(app);
-const RedisStore = require("connect-redis")(session);
 require("dotenv").config();
+
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    credentials: "true",
-  },
+  cors: corsConfig,
 });
+
 app.use(helmet());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors(corsConfig));
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.COOKIE_SECRET,
-    credentials: true,
-    name: "sid",
-    store: new RedisStore({ client: redisClient }),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: process.env.ENVIRONMENT === "production" ? "true" : "auto",
-      httpOnly: true,
-      expires: 1000 * 60 * 60 * 24 * 7,
-      sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
-    },
-  })
-);
+app.use(sessionMiddleware);
 app.use("/auth", authRouter);
 
-io.on("connect", socket => {});
+io.use(wrap(sessionMiddleware));
+io.on("connect", socket => {
+  console.log(socket.request.session.user.username);
+});
 
 server.listen(4000, () => {
   console.log("Server listening on port 4000");
