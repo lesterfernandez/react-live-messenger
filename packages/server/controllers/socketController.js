2b4b7658f0a7bc3a62abc3acbdf5436fe7afe55f
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-12-19 15:02:31 -0500

    15.persisting.socket.sessions

--- packages/server/controllers/socketController.js
+++ packages/server/controllers/socketController.js
@@ -1,9 +1,16 @@
+const redisClient = require("../redis");
+
 module.exports.authorizeUser = (socket, next) => {
   if (!socket.request.session || !socket.request.session.user) {
     console.log("Bad request!");
     next(new Error("Not authorized"));
   } else {
     socket.user = { ...socket.request.session.user };
+    redisClient.hset(
+      `userid:${socket.user.username}`,
+      "userid",
+      socket.user.userid
+    );
     next();
   }
 };
