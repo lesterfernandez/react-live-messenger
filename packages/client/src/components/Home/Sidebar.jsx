41313e58ecde1a5c888bc831431186a06d6e2740
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-12-24 03:09:43 -0500

    prod

--- packages/client/src/components/Home/Sidebar.jsx
+++ packages/client/src/components/Home/Sidebar.jsx
@@ -31,7 +31,11 @@ const Sidebar = () => {
           {friendList.map(friend => (
             <HStack as={Tab} key={`friend:${friend}`}>
               <Circle
-                bg={friend.connected ? "green.700" : "red.500"}
+                bg={
+                  friend.connected.toString() === "true"
+                    ? "green.700"
+                    : "red.500"
+                }
                 w="20px"
                 h="20px"
               />
